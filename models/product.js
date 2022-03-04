677467095e5d26e660adb6188271c1abdc7ed9b4
izzulputra <izzulislam97@gmail.com>
2022-03-04 15:23:34 +0700

    add farm Model and Schema

--- models/product.js
+++ models/product.js
@@ -1,8 +1,10 @@
 //File yang digunakan untuk membentuk schema dan prasyarat untuk bisa diakses. kemudian di export agar bisa diambil oleh seeds.js
 
 const mongoose = require('mongoose');
+const { Schema } = mongoose; // Schema is object property of mongoose
+//it's can Schema : Schema but they can write {Schema}. Then u can use the Schema to create new blank object with new
 
-const productSchema = new mongoose.Schema({
+const productSchema = new Schema({
     name: {
         type: String,
         required: true
@@ -16,7 +18,12 @@ const productSchema = new mongoose.Schema({
         type: String,
         lowercase: true,
         enum: ['fruit', 'vegetable', 'dairy']
+    },
+    farm: {
+        type: Schema.Types.ObjectId,
+        ref: 'Farm'
     }
+
 })
 
 //mengakses dan membuat collection
