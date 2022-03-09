b3228dc2d443715ee3f3d4dc84c75705a3dfae38
izzulputra <izzulislam97@gmail.com>
2022-03-09 10:33:13 +0700

    add show pages

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ app.post('/farms', async (req,res)=> {
         // res.send(req.body)
 })
 
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id);
+    res.render('farms/show', { farm });
+})
+
 
 
 
